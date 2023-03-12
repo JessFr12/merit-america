1ba4e512e1ec02d3ac4a33fd6d27e9135ad3784f
JessFr12 <tx.franco6@gmail.com>
2023-03-12 07:38:43 -0500

    Setup BankAccount and start CheckingAccount

---
+++ 11_Inheritance/exercise/src/main/java/com/techelevator/CheckingAccount.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+public class CheckingAccount extends BankAccount {
+
+    //private static final int OVERDRAFT_FEE = 10;
+    //private static final int OVERDRAFT_LIMIT = -100;
+
+    public CheckingAccount(String accountHolderName, String accountNumber) {
+        super(accountHolderName,accountNumber);
+    }
+
+    public CheckingAccount(String accountHolderName, String accountNumber, int balance) {
+        super(accountHolderName,accountNumber,balance);
+    }
+
+    public int withdraw(int amountToWithdraw) {
+        return 0;
+
+    }
+
+
+}
