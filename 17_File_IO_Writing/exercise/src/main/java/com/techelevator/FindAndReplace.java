508cd009aa7e3588532cef358c14e52dd06b1d0e
JessFr12 <tx.franco6@gmail.com>
2023-03-28 17:44:29 -0500

    Pass FindAndReplace tests

--- 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,8 +1,50 @@
 package com.techelevator;
 
+
+import java.io.*;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
     public static void main(String[] args) {
+
+        Scanner user = new Scanner(System.in);
+
+        System.out.println("What is the search word? ");
+        String searchWord = user.nextLine();
+
+        System.out.println("What is the replacement word? ");
+        String replaceWord = user.nextLine();
+
+        System.out.println("What is the source file?");
+        String sourcePath = user.nextLine();
+
+        System.out.println("What is the destination file? ");
+        String destinationPath = user.nextLine();
+
+        File sourceFile = new File(sourcePath);
+
+        File destFile = new File(destinationPath);
+
+        StringBuilder sb = new StringBuilder();
+
+        try(Scanner inputFile = new Scanner(sourceFile)) {
+            while (inputFile.hasNextLine()) {
+                String line = inputFile.nextLine();
+                sb.append(line);
+                sb.append(System.lineSeparator());
+            }
+        } catch (FileNotFoundException fnf) {
+            System.out.println("File does not exist: " + sourceFile.getAbsolutePath());
+        }
+
+        String updateText = sb.toString().replaceAll(searchWord,replaceWord);
+
+        try(PrintWriter outputFile = new PrintWriter(new FileOutputStream(destFile))) {
+            outputFile.println(updateText);
+        } catch (FileNotFoundException fnf) {
+            System.out.println("Invalid destination file: " + destFile.getAbsolutePath());
+        }
 
     }
 
